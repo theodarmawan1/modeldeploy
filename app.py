bd41bfc378a4962708b404e7c7448d28de7bf63b
theo delfi <theot@MSI>
2024-04-16 22:09:54 +0800

    second

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as stc
 
+from ml_app import run_ml_app
+
 html_temp = """
             <div style="background-color:#3872fb;padding:10px;border-radius:10px"> 
                 <h1 style="color:white;text-align:center;">Employee Promotion Prediction App</h1>
@@ -16,19 +18,21 @@ desc_temp = """
             """
 
 def main():
+    # st.title("Main App")
     stc.html(html_temp)
 
-    menu = ["Home","Machine Learning","Exploratory Data Analysis"]
+    menu = ["EDA","Model Section"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
         st.subheader("Home")
         st.markdown(desc_temp, unsafe_allow_html=True)
     elif choice == "EDA":
-        print("eda")
+        print("result")
     elif choice == "Model Section":
-        print("model section")
-        
+        run_ml_app()
+    
+
 if __name__ == '__main__':
     main()
 
